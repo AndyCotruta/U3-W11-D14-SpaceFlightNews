d177bbc6f324890361b1880edf9241ba845bff91
AndyCotruta <acotruta18@gmail.com>
2022-12-08 18:13:05 +0100

    updatednavbar

--- src/components/MyNav.tsx
+++ src/components/MyNav.tsx
@@ -1,20 +1,33 @@
 import { Navbar, Nav, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
-const MyNav = () => (
-  <Navbar className="navbar" variant="dark" expand="lg">
-    <Link to={"/"}>
-      <Navbar.Brand>SpaceFlight News</Navbar.Brand>
-    </Link>
+import { Link, useLocation } from "react-router-dom";
+const MyNav = () => {
+  const location = useLocation();
 
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Link to={"/"}>
-          <div className="navlink">Home</div>
-        </Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-);
+  return (
+    <Navbar className="navbar" variant="dark" expand="lg">
+      <Link to={"/"}>
+        <Navbar.Brand>SpaceFlight News</Navbar.Brand>
+      </Link>
+
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Link to={"/"}>
+            <div
+              className={
+                location.pathname === "/" ? "nav-link active" : "nav-link"
+              }
+            >
+              Home
+            </div>
+          </Link>
+          <Link to={"/"}>
+            <div className="nav-link">Contact Us</div>
+          </Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
 export default MyNav;
